af4f746d9fdf77922b70635caa6b63bac2da76e4
Micael Macedo <micaelm2009@hotmail.com>
2022-08-24 00:32:36 +0000

    Feat: add project

---
+++ POM/src/main/java/com/demo/site/Site.java
@@ -0,0 +1,13 @@
+package com.demo.site;
+
+import java.util.Properties;
+
+public class Site {
+	public String nomeNavegador;
+	public String urlSite;
+	public Properties propriedades;
+	
+	public Site() {
+		
+	}
+}
